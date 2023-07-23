648e925d61fc1a833f3770e83f63d5f0bf0a8f91
amirreza017 <amirreza.forootan@outlook.com>
2023-07-23 14:49:47 +0330

    Bug Fixed

--- script.js
+++ script.js
@@ -23,9 +23,61 @@ let cards = { Lines: lines };
 console.log('cards ::',cards)
 
 if (Array.isArray(cards.Lines)) {
-    cards.Lines.forEach((index) => {
-        document.getElementsByClassName('todo-list-card').innerHTML += `
+    let html = '';
+
+    cards.Lines.forEach((line,index) => {
+        let boxcard = document.getElementsByClassName('todo-list');
+    let div = document.createElement('div');
+    div.classList.add('todo-list-card');
+
+    div.innerHTML += `
         <label for="checkbox" id="label-checkbox">${cards.Lines[index]}</label>`;
+
+    // Checkbox
+    let checkbtn = document.createElement('div');
+    checkbtn.innerHTML = `<input type="checkbox" id="checkbox">`
+    checkbtn.addEventListener('click', (e) => {
+        e.target.parentElement.parentElement.children[0].classList.toggle('label-checkedbox');
+    })
+
+    // Delete
+    let deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('id','btn-trash');
+    deleteBtn.innerHTML = `<img src="./images/icons8-trash.svg" id="trash" />`;
+    deleteBtn.addEventListener('click', () => {
+        modifyElement(deleteBtn);
+    })
+
+    //Edit
+    let editBtn = document.createElement('button');
+    editBtn.setAttribute('id','btn-edit');
+    editBtn.innerHTML = `<img src="./images/icons8-edit-64.png" id="edit" />`;
+    editBtn.addEventListener('click', (e) => {
+        // editBool = true;
+        // modifyElement(editBtn, true);
+        e.target.parentElement.parentElement.children[0].contentEditable = true;
+        e.target.parentElement.parentElement.children[0].classList.add('label-editable');
+        e.target.parentElement.parentElement.children[1].children[0].disabled = true;
+        e.target.parentElement.parentElement.children[4].classList.remove('hide');
+        
+    })
+
+    // Edit Done
+    let editedBtn = document.createElement('button');
+    editedBtn.setAttribute('id','btn-edit-done');
+    editedBtn.setAttribute('class','hide');
+    editedBtn.innerHTML = `<img src="./images/icons8-done.svg" id="btn-edit-done-svg" />`
+    editedBtn.addEventListener('click',(e) => {
+        e.target.parentElement.parentElement.children[0].contentEditable = false;
+        e.target.parentElement.parentElement.children[0].classList.remove('label-editable')
+        e.target.parentElement.parentElement.children[1].children[0].disabled = false;
+        e.target.parentElement.classList.add('hide')
+    })
+    div.appendChild(checkbtn);
+    div.appendChild(deleteBtn);
+    div.appendChild(editBtn);
+    div.appendChild(editedBtn);
+    boxcard[0].appendChild(div);
     })
 }
 
@@ -108,5 +160,3 @@ const modifyElement = (element,edit = false) => {
     }
     parentDiv.remove();
 }
-var obj = JSON.parse('{ "name":"John", "age":30, "city":"New York"}');
-console.log(obj)
